8838a5d79dc9d254a0aef9990ae8837f0b0e033b
Mahad Abbas <mahadabbas31@gmail.com>
2026-03-17 09:57:29 +0500

    Lab 07 Practice

--- 2501366_MahadAbbas_L7.cpp
+++ 2501366_MahadAbbas_L7.cpp
@@ -1,25 +1,73 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// class decrement{
+//     int val;
+//     public:
+//     decrement(){
+//         val=5;
+//     }
+//     decrement operator--(int){
+//         decrement temp;
+//         val--;
+//         return temp;
+//     }
+//     void display(){
+//         cout<<"value is: "<<val<<endl;
+//     }
+// };
+// int main()
+// {
+//     decrement obj,result;
+//     result=obj--;
+//     result.display();
+//     obj.display();
+//     return 0;
+// }
+
+
+#include<iostream>
 using namespace std;
-class decrement{
-    int val;
-    public:
-    decrement(){
-        val=5;
+
+class BankAccount{
+private:
+    int balance;
+    int reward;
+
+public:
+    void setData(){
+        cout << "Enter balance: ";
+        cin >> balance;
+        cout << "Enter reward points: ";
+        cin >> reward;
     }
-    decrement operator--(int){
-        decrement temp;
-        val--;
-        return temp;
+
+    void display() const{
+        cout << "Balance: " << balance << endl
+             << "Reward Points: " << reward << endl;
     }
-    void display(){
-        cout<<"value is: "<<val<<endl;
+
+    BankAccount operator+(BankAccount obj2){
+    
+        BankAccount obj3;
+        obj3.balance = balance + obj2.balance;
+        obj3.reward = reward + obj2.reward;
+
+        return obj3;   // Return the original state before increment
     }
+
 };
-int main()
-{
-    decrement obj,result;
-    result=obj--;
-    result.display();
-    obj.display();
-    return 0;
+
+int main(){
+    BankAccount obj1, obj2, total;
+
+    cout << "Enter details for Account 1:\n";
+    obj1.setData();
+
+    cout << "Enter details for Account 2:\n";
+    obj2.setData();
+
+    total = obj1 + obj2;  // Use the overloaded + operator
+    cout << "Total Account Details:\n";
+    total.display();
+
 }
