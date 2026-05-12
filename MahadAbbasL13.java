ef29fd56a466e7d48edc036daf6a9e628d8bf8c6
Mahad Abbas <mahadabbas31@gmail.com>
2026-05-12 21:34:37 +0500

    OOP Lab 13

---
+++ MahadAbbasL13.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class MahadAbbasL13 {
+
+    // public static void access(int arr[])throws Exception{
+    //     System.out.println(arr[11]);
+    // }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        // String name;
+        // int id;
+        // float gpa;
+        // char grade;
+
+        // System.out.print("Enter Name: ");
+        // name = scanner.nextLine();
+        
+        // System.out.print("Enter ID: ");
+        // id = scanner.nextInt();
+        
+        // System.out.print("Enter CGPA: ");
+        // gpa = scanner.nextFloat();
+        
+        // scanner.nextLine();
+        // System.out.print("Enter Grade: ");
+        // grade = scanner.nextLine().charAt(0);
+        
+        // System.out.println("Name: " + name);
+        // System.out.println("ID: " + id);
+        // System.out.println("Grade: " + grade);
+        // System.out.println("gpa: " + gpa);
+        
+        // double x, y=0;
+        // System.out.println("\nEnter 2 No.(s): ");
+        // x = scanner.nextDouble();
+        // y = scanner.nextDouble();
+        
+        // int x, y=0;
+        // System.out.println("\nEnter No.(s): ");
+        // x = scanner.nextInt();
+        // // y = scanner.nextDouble();
+        
+        // System.out.println("x/y = " + x/y);
+
+        // try{
+        //     System.out.println("Result: " + x/y);
+            
+        // }
+        // catch(Exception e){
+
+        // }
+        
+        int arr[] = new int[10];
+        try{
+            // System.out.;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e.getMessage());
+        }
+        
+        
+        // try {
+
+        //     System.out.println(arr[11]);
+
+        // }
+        // catch (ArrayIndexOutOfBoundsException e) {
+
+        //     System.out.println("Handled Successfully");
+        //     System.out.println(e.getMessage());
+
+        // }
+
+        System.out.println("Program still running");
+
+        scanner.close();
+
+    }    
+}
